abbc46a0c00bdd9ef06608709c6ac3a3fa1928f3
Parijat Mazumdar <mazumdarparijat@gmail.com>
2016-01-25 00:40:12 +0530

    get movies data from endpoint; custom cells

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Parijat Mazumdar on 25/01/16.
+//  Copyright © 2016 Parijat Mazumdar. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
